f070691fb64dd5cc124d25473a57552df4745483
unluckypleasure <unluckypleasure@yandex.ru>
2024-12-14 23:51:11 +0300

    ts eslint mocks

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,4 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
+/* eslint-disable */
+
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    throw new Error('Missing Supabase environment variables');
+}
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
