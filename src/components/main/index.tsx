a3da7d349da08169497312e6412b482c94614b3d
unluckypleasure <unluckypleasure@yandex.ru>
2024-12-15 12:57:15 +0300

    Update form component with responsive min-width for mobile and desktop views + pravka

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -16,7 +16,7 @@ export function Component({ data }: { data: any[] }) {
             key={item.id}
             className="font-mono p-4  min-w-[39vw]  bg-gray-50 border border-gray-300 text-gray-900 focus:ring-0 focus:outline-none text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
-            <div className="flex flex-col gap-4 mx-[2vw] ">
+            <div className="flex flex-col gap-4  ">
               <p>{'#' + item.id + ` by ${item.name}`}</p>
               <p>{item.message}</p>
             </div>
