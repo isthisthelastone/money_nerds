3aca760c1e3d583d64909674da912ad3a7299683
unluckypleasure <unluckypleasure@yandex.ru>
2024-12-15 12:53:37 +0300

    Update form component with responsive min-width for mobile and desktop views

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -64,13 +64,13 @@ export function FormContent() {
   };
 
   return (
-    <form className="flex flex-col p-4 gap-4">
+    <form className="flex flex-col gap-4">
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
         onKeyDown={handleSubmit}
-        className="resize-none max-w-[15vw] min-h-[4vh] bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-0 focus:outline-none rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="resize-none max-w-[35vw] md:max-w-[15vw] min-h-[4vh] bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-0 focus:outline-none rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Enter your name"
         required
       />
@@ -78,7 +78,7 @@ export function FormContent() {
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         onKeyDown={handleSubmit}
-        className="resize-none min-w-[39vw] min-h-[10vh] bg-gray-50 border border-gray-300 text-gray-900 focus:ring-0 focus:outline-none text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="resize-none  md:max-w-[39vw]  md:min-w-[39vw] max-w-[79vw] min-w-[79vw] min-h-[10vh] bg-gray-50 border border-gray-300 text-gray-900 focus:ring-0 focus:outline-none text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Type here to get money"
         required
       />
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@/components';
 export function Component({ data }: { data: any[] }) {
   return (
    <main className="flex flex-col items-center justify-start gap-16 min-h-screen w-full px-4">
-      <h1 className="text-4xl text-white pt-[20vh]">MONEY NERDS</h1>
+      <h1 className="text-[12vw] text-white pt-[20vh]">MONEY NERDS</h1>
 
       {/* Form for Client-side Interaction */}
       <Form />
@@ -14,7 +14,7 @@ export function Component({ data }: { data: any[] }) {
         {data.map((item) => (
           <li
             key={item.id}
-            className="font-mono p-4  min-w-[39vw] bg-gray-50 border border-gray-300 text-gray-900 focus:ring-0 focus:outline-none text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="font-mono p-4  min-w-[39vw]  bg-gray-50 border border-gray-300 text-gray-900 focus:ring-0 focus:outline-none text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <div className="flex flex-col gap-4 mx-[2vw] ">
               <p>{'#' + item.id + ` by ${item.name}`}</p>
